854c5c8fc6f1b30322e91152a90a02db51f630ce
Blake Kostner <btkostner@gmail.com>
2019-07-25 11:43:23 -0600

    bug: export PopTerminal from package

--- src/components.js
+++ src/components.js
@@ -3,4 +3,7 @@
  * Exports a bunch of helpful Pop!_OS Vue components
  */
 
+export { default as PopTerminal } from './components/terminal.vue'
+
+export { default as PopWindowContainer } from './components/window-container.vue'
 export { default as PopWindowHeader } from './components/window-header.vue'
